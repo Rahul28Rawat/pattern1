b94a09667972bfc61dc466ce24b7442dc321a31b
Gagan Arora <arorag2577@gmail.com>
2023-03-31 18:06:09 +0530

    input var

--- SquarePattern.cpp
+++ SquarePattern.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for (int i = 0; i < 5; i++)
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < n; j++)
         {
             cout<<"* ";
         }
